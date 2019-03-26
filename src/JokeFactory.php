65ffeeb3297b64bde40949576633c93f1331d596
Roberto Luna Rojas <roberto.luna.rojas@gmail.com>
2019-03-26 04:07:56 +0000

    Apply fixes from StyleCI

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -18,7 +18,7 @@ class JokeFactory
     public function getRandomJoke()
     {
         $response = $this->client->get(self::API_ENDPOINT);
-        $joke =  json_decode($response->getBody()->getContents());
+        $joke = json_decode($response->getBody()->getContents());
 
         return $joke->value->joke;
     }
