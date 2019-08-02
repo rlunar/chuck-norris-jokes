6d81dd51bbbbbc4149c52d0a1ac7c2e528d12f02
Roberto Luna Rojas <rluna@pch.com>
2019-08-02 15:08:00 -0400

    24 - Writing Tests For Our Laravel Command

---
+++ tests/LaravelTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Rluna\ChuckNorrisJokes\Tests;
+
+use Illuminate\Support\Facades\Artisan;
+use Orchestra\Testbench\TestCase;
+use Rluna\ChuckNorrisJokes\ChuckNorrisJokesServiceProvider;
+use Rluna\ChuckNorrisJokes\Facades\ChuckNorris;
+
+class LaravelTest extends TestCase
+{
+    protected function getPackageProviders($app)
+    {
+        return [
+            ChuckNorrisJokesServiceProvider::class,
+        ];
+    }
+
+    protected function getPackageAliases($app)
+    {
+        return [
+            'ChuckNorris' => ChuckNorris::class,
+        ];
+    }
+
+    /** @test */
+    public function the_console_command_returns_a_joke()
+    {
+        $this->withoutMockingConsoleOutput();
+        ChuckNorris::shouldReceive('getRandomJoke')
+                    ->once()
+                    ->andReturn('Chuck Norris can instantiate an abstract class.');
+        $this->artisan('chuck-norris');
+        $output = Artisan::output();
+        $this->assertSame('Chuck Norris can instantiate an abstract class.'.PHP_EOL, $output);
+    }
+}
