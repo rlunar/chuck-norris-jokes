9294f20ae75be262a660b6ba38e86cfbccd1f8b0
Roberto Luna Rojas <rluna@pch.com>
2019-08-02 23:13:45 -0400

    28 - Adding Database Migrations

--- src/ChuckNorrisJokesServiceProvider.php
+++ src/ChuckNorrisJokesServiceProvider.php
@@ -27,6 +27,12 @@ class ChuckNorrisJokesServiceProvider extends ServiceProvider
             __DIR__.'/../config/chuck-norris.php' => base_path('config/chuck-norris.php'),
         ], 'config');
 
+        if (!class_exists('CreateJokesTable')) {
+            $this->publishes([
+                __DIR__.'/../database/migrations/create_jokes_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_jokes_table.php'),
+            ], 'migrations');
+        }
+
         Route::get(config('chuck-norris.route'), ChuckNorrisController::class);
     }
 
---
+++ src/Models/Joke.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Rluna\ChuckNorrisJokes\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Joke extends Model
+{
+    protected $table = 'jokes';
+
+    protected $guarded = [];
+}
